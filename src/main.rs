dfa7d5dae4dfeb7aeec53656a07ac4300599779e
Jeremy Borgman <borgman.jeremy@pm.me>
2020-08-28 11:09:07 -0500

    added sound test

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,51 @@
-use gba::cpu::*;
-use gba::memory::*;
+use sdl2::audio::{AudioCallback, AudioSpecDesired};
+use std::time::Duration;
 
+struct SquareWave {
+    phase_inc: f32,
+    phase: f32,
+    volume: f32,
+}
+
+impl AudioCallback for SquareWave {
+    type Channel = f32;
+
+    fn callback(&mut self, out: &mut [f32]) {
+        // Generate a square wave
+        for x in out.iter_mut() {
+            *x = if self.phase <= 0.5 {
+                self.volume
+            } else {
+                -self.volume
+            };
+            self.phase = (self.phase + self.phase_inc) % 1.0;
+        }
+    }
+}
 fn main() {
-    let mem = MemoryMap::new(0xFFFF);
-    let _cpu = Cpu::new(&mem);
+    let sdl_context = sdl2::init().unwrap();
+    let audio_subsystem = sdl_context.audio().unwrap();
+
+    let desired_spec = AudioSpecDesired {
+        freq: Some(44100),
+        channels: Some(1), // mono
+        samples: None,     // default sample size
+    };
+
+    let device = audio_subsystem
+        .open_playback(None, &desired_spec, |spec| {
+            // initialize the audio callback
+            SquareWave {
+                phase_inc: 440.0 / spec.freq as f32,
+                phase: 0.0,
+                volume: 0.25,
+            }
+        })
+        .unwrap();
+
+    // Start playback
+    device.resume();
+
+    // Play for 2 seconds
+    std::thread::sleep(Duration::from_millis(2000));
 }
