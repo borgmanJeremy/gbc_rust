8e83cbfa75dd2c008fa066ed8f8d659e1f4c556e
Jeremy Borgman <borgman.jeremy@pm.me>
2020-08-28 19:35:19 -0500

    example of using lock

--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,62 @@
+
+extern crate sdl2;
+extern crate rand;
+
 use sdl2::audio::{AudioCallback, AudioSpecDesired};
 use std::time::Duration;
 
-struct SquareWave {
-    phase_inc: f32,
-    phase: f32,
-    volume: f32,
+struct MyCallback {
+    volume: f32
 }
-
-impl AudioCallback for SquareWave {
+impl AudioCallback for MyCallback {
     type Channel = f32;
 
     fn callback(&mut self, out: &mut [f32]) {
-        // Generate a square wave
+        use self::rand::{Rng, thread_rng};
+        let mut rng = thread_rng();
+
+        // Generate white noise
         for x in out.iter_mut() {
-            *x = if self.phase <= 0.5 {
-                self.volume
-            } else {
-                -self.volume
-            };
-            self.phase = (self.phase + self.phase_inc) % 1.0;
+            *x = (rng.gen_range(0.0, 2.0) - 1.0) * self.volume;
         }
     }
 }
-fn main() {
-    let sdl_context = sdl2::init().unwrap();
-    let audio_subsystem = sdl_context.audio().unwrap();
+
+fn main() -> Result<(), String> {
+    let sdl_context = sdl2::init()?;
+    let audio_subsystem = sdl_context.audio()?;
 
     let desired_spec = AudioSpecDesired {
-        freq: Some(44100),
-        channels: Some(1), // mono
-        samples: Some(4096),     // default sample size
+        freq: Some(44_100),
+        channels: Some(1),  // mono
+        samples: None,      // default sample size
     };
 
-    let device = audio_subsystem
-        .open_playback(None, &desired_spec, |spec| {
-            // initialize the audio callback
-            SquareWave {
-                phase_inc: 440.0 / spec.freq as f32,
-                phase: 0.0,
-                volume: 0.25,
-            }
-        })
-        .unwrap();
+    // None: use default device
+    let mut device = audio_subsystem.open_playback(None, &desired_spec, |spec| {
+        // Show obtained AudioSpec
+        println!("{:?}", spec);
+
+        MyCallback { volume: 0.5 }
+    })?;
 
     // Start playback
     device.resume();
 
-    // Play for 2 seconds
-    std::thread::sleep(Duration::from_millis(2000));
+    // Play for 1 second
+    std::thread::sleep(Duration::from_millis(1_000));
+
+    {
+        // Acquire a lock. This lets us read and modify callback data.
+        let mut lock = device.lock();
+        (*lock).volume = 0.15;
+        // Lock guard is dropped here
+    }
+
+    // Play for another second
+    std::thread::sleep(Duration::from_millis(1_000));
+
+    // Device is automatically closed when dropped
+
+    Ok(())
 }
